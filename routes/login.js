a893c9febfaf762b9a942d229714cf4214dbf65f
kiyoung92 <go3go1go2@gmail.com>
2023-06-10 01:17:39 +0900

    홈택스 로그인 API

--- routes/login.js
+++ routes/login.js
@@ -13,19 +13,10 @@ router.get('/', async (req, res) => {
     let publicCertInfo = new crypto.X509Certificate(fs.readFileSync(publicCertPath));
     let publicCertSN = publicCertInfo.serialNumber.toLocaleLowerCase();
     let nowDate = new Date();
-    let hashDate = `${nowDate.getFullYear()}${nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1}${nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate.getDate()}${nowDate.getHours() < 10 ? '0' + nowDate.getHours() : nowDate.getHours()}${nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()}${nowDate.getSeconds() < 10 ? '0' + nowDate.getSeconds() : nowDate.getSeconds()}`
+    let hashDate = `${nowDate.getFullYear()}${nowDate.getMonth() + 1 < 10 ? '0' + (nowDate.getMonth() + 1) : nowDate.getMonth() + 1}${nowDate.getDate() < 10 ? '0' + nowDate.getDate() : nowDate.getDate()}${nowDate.getHours() < 10 ? '0' + nowDate.getHours() : nowDate.getHours()}${nowDate.getMinutes() < 10 ? '0' + nowDate.getMinutes() : nowDate.getMinutes()}${nowDate.getSeconds() < 10 ? '0' + nowDate.getSeconds() : nowDate.getSeconds()}`;
     let pem = `-----BEGIN CERTIFICATE-----\n${publicCert.match(new RegExp('.{1,64}', 'g')).join('\n')}\n-----END CERTIFICATE-----`;
     let privateCertInfo = new Kp(fs.readFileSync(publicCertPath), fs.readFileSync(privateCertPath), state.SET_INFO.CERTIFICATE_PASSWORD);
     let randomValue = Buffer.from(privateCertInfo.privateCertificate.random.valueBlock.valueHex, 'utf8').toString('base64');
-    // state.SET_INFO = {
-    //     BROWSER_PATH: '',           // puppeteer에 사용할 브라우저 경로
-    //     PUBLIC_CERT_PATH: '',       // 공동인증서 .der 파일 경로
-    //     PRIVATE_CERT_PATH: '',      // 공동인증서 .key 파일 경로
-    //     PUBLIC_CERT_FILE_NAME: '',  // 공동인증서 .der 파일 이름
-    //     PRIVATE_CERT_FILE_NAME: '', // 공동인증서 .key 파일 이름
-    //     CERTIFICATE_PASSWORD: '',   // 공동인증서 비밀번호
-    //     PUPPETEER_HEADLESS: '',     // puppeteer headless ('new' or true or false)
-    // }
 
     const bs = await pp.launch({
         executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
@@ -33,7 +24,7 @@ router.get('/', async (req, res) => {
             width: 1000,
             height: 1000,
         },
-        headless: false,
+        headless: state.SET_INFO.PUPPETEER_HEADLESS,
     });
 
     const htxPg = await bs.newPage();
@@ -41,4 +32,38 @@ router.get('/', async (req, res) => {
     await htxPg.goto('https://hometax.go.kr/websquare/websquare.html?w2xPath=/ui/pp/index.xml');
 
     await htxPg.waitForSelector('a[class="w2group ico_wa"]');
-})
+
+    const htxSession = await htxPg.evaluate(async () => {
+        const response = await fetch('/wqAction.do?actionId=ATXPPZXA001R01&screenId=UTXPPABA01', {method: 'POST'}).then(res => {
+            console.log(res.headers.get('Cookie'))
+            return res.text();
+        }).then(data => {
+            const parser = new DOMParser();
+            const dom = parser.parseFromString(data, 'text/html');
+            const cookies = document.cookie.split(';').map(item => item.split('=')[1]);
+
+            return { 'SSN': dom.documentElement.querySelector('pkcEncSsn').innerHTML, 'WMONID': cookies[0], 'TXPPsessionID': cookies[1] };
+        });
+
+        return response;
+    });
+
+    const bufferSSN = Buffer.from(htxSession.SSN);
+    const digitalSign = crypto.sign('RSA-SHA256', bufferSSN, privateCertInfo.privateCertificate.decryptedNativePrivateKey);
+    const signature = digitalSign.toString('base64');
+    const logSgnt = Buffer.from(`${htxSession.SSN}$${publicCertSN}$${hashDate}$${signature}`, 'utf8').toString('base64');
+
+    const htxLoginResult = await htxPg.evaluate(async data => {
+        const params = { cert: data.cert, logSgnt: data.logSgnt, pkcLgnClCd: '04', pkcLoginYnImpv: 'Y', randomEnc: data.randomValue, WMONID: data.WMONID, NTS_LOGIN_SYSTEM_CODE_P: 'TXPP', TXPPsessionID: data.TXPPsessionID };
+        const htxLogin = await fetch(`/pubcLogin.do?domain=hometax.go.kr&mainSys=Y&${new URLSearchParams(params)}`, { method: 'POST' }).then(res => {
+            return res.text();
+        }).then(data => {
+            let trimData = decodeURIComponent(data).replace(/\s/g,'').replace(/'/g, '"');
+            let sliceStr = trimData.slice(trimData.indexOf('{'), trimData.indexOf('"err')) + trimData.slice(trimData.indexOf('n"),"lgn') + 4, trimData.indexOf('}') + 1);
+            return JSON.parse(sliceStr);
+        });
+        return htxLogin;
+    }, { 'cert': pem, 'logSgnt': logSgnt, 'randomValue': randomValue, 'WMONID': htxSession.WMONID, 'TXPPsessionID': htxSession.TXPPsessionID });
+
+    console.log(htxLoginResult)
+});
